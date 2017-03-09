c9dce44b651b8a30e159b0cf8e606c701954824f
PK <22e889d8@opayq.com>
2017-03-09 10:57:43 +0000

    Added automake and autoconf files for src and tests. Added main program source file

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+
+int main(int argc, const char *argv[]) {
+    char *paths[3];
+    paths[0] = "b1";
+    paths[1] = "b2";
+    paths[2] = "b3";
+    paths[3] = NULL;
+
+
+    struct Node *root = init_node("root");
+    root->name = "root";
+    root->children_i = 0;
+    root->children_size = 1;
+    root->children = NULL;
+
+
+
+    printf("Root node %s, index %zu\n", root->name, root->children_i);
+    insert_paths(root, paths);
+    paths[0] = "b1";
+    paths[1] = "b12";
+    paths[2] = "b13";
+    paths[3] = NULL;
+    insert_paths(root, paths);
+    paths[0] = "b1";
+    paths[1] = "b22";
+    paths[2] = "b23";
+    paths[3] = NULL;
+    insert_paths(root, paths);
+    print_index(root);
+    printf("Root index before clear %zu\n", root->children_i);
+    clear(root);
+    if(!root) printf("Index cleared");
+    return 0;
+}
